a2d6e126341d5c0d44261a7120ed2da5cca889be
MykytaKotovSS <m.kotov@sloboda-studio.com>
2024-07-04 18:52:56 +0300

    add about page

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "components/Header";
 import Footer from "components/Footer";
+import About from "pages/About";
 
 const App = () => {
   return (
     <Router>
       <Header />
       <Routes>
-        <Route path="/about-us" element={<p>About us</p>} />
+        <Route path="/about-us" element={<About />} />
         <Route path="/services" element={<p>Services</p>} />
         <Route path="/sustainability" element={<p>Sustainability</p>} />
         <Route path="/contact-us" element={<p>Contact us</p>} />
---
+++ src/pages/About/About.jsx
@@ -0,0 +1,26 @@
+import "./About.scss";
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="about">
+      <article className="about-us">
+        <div className="block container">
+          <h1>About LibraLink</h1>
+          <p>
+            LibraLink is dedicated to providing cutting-edge datacenter
+            solutions that prioritize both performance and <br />
+            environmental responsibility. Our commitment to using green energy
+            ensures that we deliver powerful, reliable <br /> services without
+            compromising the planet’s future. With facilities in Luxembourg and
+            Belgium, we are <br /> strategically positioned to serve clients
+            across Europe and beyond.
+          </p>
+        </div>
+      </article>
+      <article>two</article>
+    </section>
+  );
+};
+
+export default About;
