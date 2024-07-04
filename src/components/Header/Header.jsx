999d1ed7dba98fe770087a4c8b1af53579c62627
MykytaKotovSS <m.kotov@sloboda-studio.com>
2024-07-04 16:35:52 +0300

    add config and icons

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,18 @@
 import "./Header.scss";
 import React from "react";
 import { Link } from "react-router-dom";
+import { ReactComponent as Logo } from "assets/icons/Command.svg";
+import { ReactComponent as Chats } from "assets/icons/Chats.svg";
 
 const Header = () => {
   return (
     <header className="header">
       <div className="menu">
         <nav className="nav">
-          <Link to="/">Libralink</Link>
-
+          <Link to="/" className="logo">
+            <Logo />
+            Libralink
+          </Link>
           <Link to="/about-us">About Us</Link>
 
           <Link to="/services">Services</Link>
@@ -16,7 +20,10 @@ const Header = () => {
           <Link to="/sustainability">Sustainability</Link>
         </nav>
         <div className="contact">
-          <Link to="/contact-us">Contact us</Link>
+          <Link to="/contact-us">
+            <Chats />
+            Contact us
+          </Link>
         </div>
       </div>
     </header>
