43089b2807f49a397e55ce5bf71c4fc04b4a5ca2
MykytaKotovSS <m.kotov@sloboda-studio.com>
2024-07-05 11:28:47 +0300

    update header active link color

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,11 @@
 import "./Header.scss";
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { ReactComponent as Logo } from "assets/icons/Command.svg";
 import { ReactComponent as Chats } from "assets/icons/Chats.svg";
 
 const Header = () => {
+  const location = useLocation();
   return (
     <header className="header">
       <div className="menu container">
@@ -13,11 +14,26 @@ const Header = () => {
             <Logo />
             Libralink
           </Link>
-          <Link to="/about-us">About Us</Link>
+          <Link
+            to="/about-us"
+            className={location.pathname === "/about-us" ? "active" : ""}
+          >
+            About Us
+          </Link>
 
-          <Link to="/services">Services</Link>
+          <Link
+            to="/services"
+            className={location.pathname === "/services" ? "active" : ""}
+          >
+            Services
+          </Link>
 
-          <Link to="/sustainability">Sustainability</Link>
+          <Link
+            to="/sustainability"
+            className={location.pathname === "/sustainability" ? "active" : ""}
+          >
+            Sustainability
+          </Link>
         </nav>
         <div className="contact">
           <Link to="/contact-us">
