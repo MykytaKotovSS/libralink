5156898a5860354062456818460b2efc2fd1432e
MykytaKotovSS <m.kotov@sloboda-studio.com>
2024-07-09 02:15:37 +0300

    update contact us page top

--- src/pages/ContactUs/ContactUs.jsx
+++ src/pages/ContactUs/ContactUs.jsx
@@ -14,6 +14,20 @@ const ContactUs = () => {
             phone, or visit our headquarters in Luxembourg for a face-to-face
             meeting.
           </p>
+          <p>
+            Email <br />
+            <span>info@libralink.com</span>
+          </p>
+          <p>
+            Phone <br />
+            <span>+352 123 456 789</span>
+          </p>
+          <p>
+            Address <br />
+            <span>LibraLink HQ, 12 Green Street,</span>
+            <br />
+            <span>Luxembourg City, Luxembourg</span>
+          </p>
         </div>
       </article>
     </section>
